74363909c5eb2b46ea4fa5ed878a4d7c2c0070c2
yoshi0319 <yossii3150@gmail.com>
2023-12-21 17:58:05 +0900

    a

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,16 @@
 
 import './App.css';
 import {Header} from './Header.jsx';
-import {Description} from './Description';'./Description.jsx';
-import {DogImage} from './DogImage.jsx';
+import {Description} from './Description.jsx';
+import {DogImage} from './DogImage.jsx'
 import React, { useState } from 'react';
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
-
-  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
-
-  const btnClick = () =>{
-    fetch("https://dog.ceo/api/breeds/image/random")
-    .then(response => response.json())
-    .then(json => setDogUrl(json.message))
-}
-
   return (
     <><Header />
-    <div>
-      <p>犬の画像を表示するサイトです。</p>
-      <img src={dogUrl} />
-      <button onClick={btnClick}>更新</button>
-    </div></>
+    <Description /></>
   )
 }
--- src/Description.jsx
+++ src/Description.jsx
@@ -1,7 +1,19 @@
 // @ts-check
+import React, { useState } from 'react';
 
 export const Description = () => {
-  return <></>
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
+
+  const btnClick = () =>{
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(json => setDogUrl(json.message))
+}
+  return <><div>
+  <p>犬の画像を表示するサイトです。</p>
+  <img src={dogUrl} />
+  <button onClick={btnClick}>更新</button>
+</div></>
 }
 
 export default Description
